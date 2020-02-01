1d6eb2c7c261d0448253ed060977c0c2e946f9b7
Kasey Arnold <kaseyarnold15@gmail.com>
2020-02-01 01:30:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,16 @@
 def key_for_min_value(name_hash)
   
   
-  
+  def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |key, value|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
+end
 
 end
